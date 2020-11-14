603a51cd5e0ae0941808744cdb72a5eb30d17e9b
hououinkami <hououinkami@gmail.com>
2020-11-14 15:38:18 +0800

    变更加载顺序

--- init.lua
+++ init.lua
@@ -10,11 +10,11 @@ hotkey.bind(hyperkey, "p", hs.openPreferences)
 hotkey.bind({"option"}, "z", hs.toggleConsole)
 -- 组件加载管理
 local module_list = {
+	"module.Network",
 	"module.Music",
 	"module.Window",
 	"module.Spotlightlike",
 	"module.IME",
-	"module.Network",
 	"module.AppKeyMap",
 	--"module.DesktopWidget",
 		}
