aa86f007fd78ae978d56dd57b29b7166372bee27
hououinkami <hououinkami@gmail.com>
2023-11-03 13:30:00 +0800

    edit

--- module/test.lua
+++ module/test.lua
@@ -1,5 +1,6 @@
 win = require("hs.window")
 c = require("hs.canvas")
+as = require("hs.osascript")
 local wRe = 0.12
 local hRe = 2 / 3
 local gap = 10
@@ -41,4 +42,52 @@ function windowsControl()
 	end
     --background:show()
 end
-windowsControl()
+--windowsControl()
+
+
+
+
+local screenfullframe = hs.screen.mainScreen():fullFrame()
+local iconW,iconH = 120,110
+local icongap = 5
+local firstIconFrame = {x = screenfullframe.w * (1615 - 60 - icongap / 2) / 1680, y = screenfullframe.h * (30 - icongap / 2) / 1050, w = 120 + icongap, h = 110 + icongap}
+local geticonpositionscript = [[
+        tell application "Finder"
+	        get desktop position of every item of desktop
+        end tell
+	]]
+local _,iconPosition,_ = as.applescript(geticonpositionscript)
+local geticoncountscript = [[
+        tell application "Finder"
+	        set i to index of every item of desktop
+            get length of i
+        end tell
+	]]
+local _,iconCount,_ = as.applescript(geticoncountscript)
+local seticonvarscript = [[
+        tell application "Finder"
+            set iconvar to {}
+            set i to index of every item of desktop
+            repeat with v from 1 to (length of i)
+                set end of iconvar to "icon" & v
+            end repeat
+            return iconvar
+        end tell
+	]]
+local _,icon,_ = as.applescript(seticonvarscript)
+count = 1
+repeat
+    icon[count] = c.new({x = iconPosition[count][1] - iconW / 2, y = iconPosition[count][2] - 35 * screenfullframe.h / 1050, h = iconH, w = iconW}):level(c.windowLevels.desktopIcon + 1):replaceElements(
+        {-- 背景
+            id = "icon" .. count,
+            type = "rectangle",
+            action = "fill",
+            fillColor = {alpha = bgAlpha, red = bgColor[1] / 255, green = bgColor[2] / 255, blue = bgColor[3] / 255},
+            trackMouseEnterExit = true,
+            trackMouseUp = true
+        }
+    )
+    --icon[count]:show()
+    count = count + 1
+until count > iconCount
+
