497497eb275bc3a61a5e3bd050dedf68cdc72548
hououinkami <hououinkami@gmail.com>
2024-03-14 13:44:17 +0800

    测试

--- module/test.lua
+++ module/test.lua
@@ -1,30 +1,12 @@
-local dataa = {
-    ["music.search.SearchCgiService"]={
-        ["method"]="DoSearchForQQMusicDesktop",
-        ["module"]="music.search.SearchCgiService",
-        ["param"]={
-            ["num_per_page"]=1,
-            ["page_num"]=1,
-            ["query"]="アイドル",
-            ["search_type"]=0
-        }
-    }
-}
-musicdata = hs.json.encode(dataa)
-musicheaders = {
-				["Referer"] = "https://c.y.qq.com"
-			}
-hs.http.asyncPost("https://u.y.qq.com/cgi-bin/musicu.fcg", musicdata, musicheaders, function (musicStatus,musicBody,musicHeader)
-    if musicStatus == 200 then 
-        a = hs.json.decode(musicBody)
-        print(musicBody)
-        b = a["music.search.SearchCgiService"].data.body.song.list[1].mid
-        print(b)
-        b = a["music.search.SearchCgiService"].data.body.song.list[1].name
-        print(b)
-        b = a["music.search.SearchCgiService"].data.body.song.list[1].album.name
-        print(b)
-        b = a["music.search.SearchCgiService"].data.body.song.list[1].singer[1].name
-        print(b)
-    end
+modal = hs.hotkey.modal.new({}, "F19")
+modal:bind({}, "a", function()
+    print("123a")
+    modal:exit() -- remove if this shouldn't clear the modal toggle
+end)
+modal:bind({}, "b", function()
+    print("123b")
+    modal:exit() -- remove if this shouldn't clear the modal toggle
+end)
+modal:bind({}, "F19", function() -- second press leaves modal
+    modal:exit()
 end)
