f4e22caf12fe920cbdebd799b51b4b25ef8aa46b
hououinkami <hououinkami@gmail.com>
2024-12-24 13:38:56 +0800

    开启输入法自动切换

--- module/IME.lua
+++ module/IME.lua
@@ -6,53 +6,55 @@ for key, value in pairs(k.methods()) do
 		roma = true
 	end
 end
+-- 定义输入法
+pinyin = "com.apple.inputmethod.SCIM.ITABC"
+japanese = "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese"
 if roma == true then
-	eng = "com.apple.inputmethod.Kotoeri.Roman"
+	english = "com.apple.inputmethod.Kotoeri.Roman"
 	inputs = {"拼音 - 簡体字", "英字", "ひらがな"}
 else
-	eng = "com.apple.keylayout.ABC"
+	english = "com.apple.keylayout.ABC"
 	inputs = {"简体拼音", "ABC", "平假名"}
 end
 local script = [[tell application "System Events" to tell process "TextInputMenuAgent" to tell (1st menu bar item of menu bar 2) to {click (menu item "tomethod" of menu 1), click}]]
 -- 切换为拼音
-local function Chinese()
-	--hs.osascript.applescript(script:gsub("tomethod",inputs[1]))
-	--k.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
-	k.setMethod("Pinyin - Simplified")
+local function toChinese()
+	k.currentSourceID(pinyin)
+	-- k.setMethod("Pinyin - Simplified")
+	-- hs.osascript.applescript(script:gsub("tomethod",inputs[1]))
 end
 -- 切换为日文
-local function Japanese()
-	--hs.osascript.applescript(script:gsub("tomethod",inputs[3]))
-	--k.currentSourceID("com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese")
-	k.setMethod("Hiragana")
+local function toJapanese()
+	k.currentSourceID(japanese)
+	-- k.setMethod("Hiragana")
+	-- hs.osascript.applescript(script:gsub("tomethod",inputs[3]))
 end
 -- 切换为英文
-local function English()
-	--hs.osascript.applescript(script:gsub("tomethod",inputs[2]))
+local function toEnglish()
+	-- hs.osascript.applescript(script:gsub("tomethod",inputs[2]))
 	if roma == true then
-		k.setMethod("Romaji")
+		k.currentSourceID("com.apple.inputmethod.Kotoeri.RomajiTyping.Roman")
+		-- k.setMethod("Romaji")
 	else
-		k.currentSourceID(eng)
+		k.currentSourceID(english)
 	end
 end
 -- 切换输入法快捷键
-hs.hotkey.bind(hyper_oc, '/', Chinese)
-hs.hotkey.bind(hyper_oc, ',', English)
-hs.hotkey.bind(hyper_oc, '.', Japanese)
+hs.hotkey.bind(hyper_oc, '/', toChinese)
+hs.hotkey.bind(hyper_oc, ',', toEnglish)
+hs.hotkey.bind(hyper_oc, '.', toJapanese)
 -- 设置App对应的输入法
 local App2Ime = {
 	{'/System/Library/CoreServices/Finder.app', 'Chinese'},
 	{'/System/Library/CoreServices/Spotlight.app', 'Chinese'},
-	{'/Applications/Hammerspoon.app', 'Chinese'},
-	{'/Applications/System Preferences.app', 'Chinese'},
 	{'/Applications/Utilities/Terminal.app', 'English'},
 	{'/Applications/Visual Studio Code.app', 'English'},
 	{'/System/Applications/Music.app', 'Japanese'},
 	{'/Applications/Safari.app', 'Chinese'},
+	{'/Applications/Telegram.app', 'Chinese'},
 	{'/Applications/WeChat.app', 'Chinese'},
 	{'/Applications/QQ.app', 'Chinese'},
 	{'/Applications/企业微信.app', 'Chinese'},
-	{'/Applications/Google Chrome.app', 'Chinese'},
 	{'/Applications/Preview.app', 'Chinese'},
 	{'/Applications/Microsoft Word.app', 'Chinese'},
 	{'/Applications/Microsoft Excel.app', 'Chinese'},
@@ -98,13 +100,13 @@ function updateFocusAppInputMethod()
 		end
 	end
 	if ime == 'English' then
-		English()
+		toEnglish()
 		--k.currentSourceID(eng)
 	elseif ime == 'Chinese' then
-		Chinese()
+		toChinese()
 		--k.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
 	elseif ime == 'Japanese' then
-		Japanese()
+		toJapanese()
 		--k.currentSourceID("com.apple.inputmethod.Kotoeri.Japanese")
 	end
 end
@@ -114,5 +116,5 @@ function applicationWatcher(appName, eventType, appObject)
 		updateFocusAppInputMethod()
 	end
 end
--- appWatcher = hs.application.watcher.new(applicationWatcher)
--- appWatcher:start()
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
