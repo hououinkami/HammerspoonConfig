83e0fbe57e03169fc1f0fb3fb89eb2d21c16597a
hououinkami <hououinkami@gmail.com>
2025-10-09 13:09:18 +0800

    更新函数

--- module/autoupdate.lua
+++ module/autoupdate.lua
@@ -1,16 +1,6 @@
-local cmdArr = {
-    "cd ~/.hammerspoon && git pull",
-}
-function shell(cmd)
-    result = as.applescript(string.format('do shell script "%s"', cmd))
-end
-function runAutoScripts()
-    for key, cmd in ipairs(cmdArr) do
-        shell(cmd)
-    end
-    hs.reload()
-end
+require ('module.utils') 
+
 -- 每天 12 点检查一遍更新
-hs.timer.doAt('12:00', hs.timer.days(1), runAutoScripts):start()
+hs.timer.doAt('12:00', hs.timer.days(1), updateHammerspoon):start()
 -- 手动触发快捷键
-hotkey.bind(hyper_coc, 'u', runAutoScripts)
+hotkey.bind(hyper_coc, 'u', updateHammerspoon)
